2b60985904a8c5ab5e8577998ba4149c76b0cf82
mllrosa <mllr8sa@gmail.com>
2025-05-19 15:53:18 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ classificador_de_nivel_de_inteligencia_artificial.py
@@ -0,0 +1,19 @@
+#1. Solicita o nome do sistema de IA
+nome_ia = (input('Informe o nome do sistema de IA:'))
+
+#2. Recebe sua pontuação de performance (de 0 a 100)
+pontuacao = float(input('Informe a pontuaçao obtida pelo sistema:'))
+
+#3. Exibe a classificação do nível de inteligência com base nessa pontuação
+
+
+
+
+| Pontuação (%)       | Classificação                         |
+|---------------------|----------------------------------------|
+| 0 até 39.9          | IA em Treinamento 🍼                   |
+| 40.0 até 69.9       | IA Intermediária 🧠                   |
+| 70.0 até 89.9       | IA Otimizada 🚀                       |
+| 90.0 até 100.0      | IA Avançada (nível Skynet) 🤯         |
+| Acima de 100        | Erro: IA fora da escala! ⚠️          |
+| Abaixo de 0         | Erro: Pontuação inválida! ❌          |
