4484b428cc6fe49e71be7c20ae6837835d044d22
mllrosa <mllr8sa@gmail.com>
2025-05-20 14:35:24 -0300

    classificador de IA

--- classificador_de_nivel_de_inteligencia_artificial.py
+++ classificador_de_nivel_de_inteligencia_artificial.py
@@ -6,14 +6,20 @@ pontuacao = float(input('Informe a pontuaçao obtida pelo sistema:'))
 
 #3. Exibe a classificação do nível de inteligência com base nessa pontuação
 
+if 0 <= pontuacao <= 39.9:
+    print(f'Segundo os dados fornecidos {nome_ia}, é uma IA em Treinamento 🍼')
 
+elif 40.0 <= pontuacao <= 69.9:
+    print(f'Segundo os dados fornecidos {nome_ia}, obteve uma pontuação Intermediária 🧠 ')
 
+elif 70.0 <= pontuacao <= 89.9:
+    print(f'Informamos que {nome_ia}, é uma IA Otimizada 🚀')
+    
+elif 90.0 <= pontuacao <= 100:
+    print(f'Informamos que {nome_ia}, teve uma pontução de IA Avançada (nível Skynet 🤯 )')
 
-| Pontuação (%)       | Classificação                         |
-|---------------------|----------------------------------------|
-| 0 até 39.9          | IA em Treinamento 🍼                   |
-| 40.0 até 69.9       | IA Intermediária 🧠                   |
-| 70.0 até 89.9       | IA Otimizada 🚀                       |
-| 90.0 até 100.0      | IA Avançada (nível Skynet) 🤯         |
-| Acima de 100        | Erro: IA fora da escala! ⚠️          |
-| Abaixo de 0         | Erro: Pontuação inválida! ❌          |
+elif pontuacao > 100:
+    print(f'Informamos que houve um erro com {nome_ia}, IA fora da escala! ⚠️ ')
+
+else:
+    print(f'Informamos que houve um erro com {nome_ia}, a pontuação é inválida! ❌')
